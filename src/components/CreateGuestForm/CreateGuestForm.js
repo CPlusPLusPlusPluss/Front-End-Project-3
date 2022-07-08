abac5ff35664034cfb9e20bdc75f1045479900cc
MeesumAshary <meesumashary@gmail.com>
2022-07-08 16:20:43 -0700

    Clear out errors. Set up for project

---
+++ src/components/CreateGuestForm/CreateGuestForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CreateGuestForm(props) {
+    return (
+        <div>
+            <h1>Hello from CreateGuestForm</h1>
+        </div>
+    );
+}
+
+export default CreateGuestForm;
