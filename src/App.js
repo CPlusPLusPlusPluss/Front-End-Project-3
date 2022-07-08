66e276df23031a2a7497c461ddb53d54daa7383d
nickgiggey <nickgiggey@comcast.net>
2022-07-08 16:32:23 -0600

    remove unneeded files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import { Routes, Route, Link } from 'react-router-dom';
 import './styles/reset.css';
 import './styles/App.css';
-import Library from './components/Library';
-import Home from './components/Home';
-import PicPerDay from './components/PicPerDay';
-import NotFound from './components/NotFound';
-import logo from './styles/images/2120445.png';
 
 function App() {
   const logoStyle = {
@@ -22,25 +17,18 @@ function App() {
             <img src={logo} alt='' style={logoStyle} />
           </Link>
         </div>
-        <div className="sky-container">
-          <div className="star"></div>
-          <div className="star"></div>
-          <div className="star"></div>
-          <div className="star"></div>
-          <div className="star"></div>
-        </div>
         <ul className="link-container">
           <Link to='/library'>
-            <li className="link pic-lib-link">Earth Library</li>
+            <li className="">Earth Library</li>
           </Link>
         </ul>
       </nav>
       <main>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/picperday' element={<PicPerDay />} />
-          <Route path='/library' element={<Library />} />
-          <Route path='*' element={<NotFound />} />
+          {/* <Route path='/' element={<Home />} /> */}
+          {/* <Route path='/picperday' element={<PicPerDay />} /> */}
+          {/* <Route path='/library' element={<Library />} /> */}
+          {/* <Route path='*' element={<NotFound />} /> */}
         </Routes>
       </main>
     </section>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PicPerDay from './PicPerDay';
+import PicPerDay from './HomeBody';
 import Header from './Header';
 import '../styles/reset.css'
 import '../styles/Home.css';
